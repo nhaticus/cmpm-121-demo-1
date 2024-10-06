47a4047483c0c305400368dada34ee17a7ce1c6e
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-06 12:47:49 -0700

    Testing commit, change header and header's color

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "The Influencer Program";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+header.style.color = "#FACADE"
+
+
