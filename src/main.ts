0fb8284f257b272e77bd6d51f9785773b83036d8
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-16 14:36:47 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -17,37 +17,65 @@ button.innerText = "▶️";
 button.style.backgroundColor = "red";
 app.append(button);
 
-//Adding counter
-let viewsCount: number = 0;
-const viewsDisplay = document.createElement("div"); //Text display
-app.append(viewsDisplay);
-viewsDisplay.innerHTML = "VIEWS: " + viewsCount;
-button.addEventListener("click", () => {
-  //increment counter and update text on click
-  incrementViewsCount(1);
-});
-
-// Helper function to increase the views count along with updating the text
+// Helper functions
 function incrementViewsCount(x: number): void {
   viewsCount += x;
-  viewsDisplay.innerHTML = `VIEWS: ${viewsCount.toFixed(1)}`;
+  viewsDisplay.innerText = `VIEWS: ${viewsCount.toFixed(2)}`;
 }
 
-//Adding passive incrementing
-const counterGrowthRate: number = 1;
-
-let lastTimeStamp: number = 0;
-function continuousGrowth(timeStamp: number): void {
+function addText(text: string): HTMLDivElement {
+  const textBox = document.createElement("div");
+  textBox.innerText = text;
+  app.append(textBox);
+  return textBox;
+}
+// update fucntion
+function updateGame(timeStamp: number): void {
   if (lastTimeStamp === 0) {
     lastTimeStamp = timeStamp;
   }
-  const timeElapsed = timeStamp - lastTimeStamp;  //Time since last frame
-  const tmp = counterGrowthRate * timeElapsed / 1000; // tmp essentially equals to 1 / frame per sec
+  const timeElapsed = timeStamp - lastTimeStamp; //Time since last frame
+  const tmp = (counterGrowthRate * timeElapsed) / 1000; //Calculates the amount of frames passed in respect to
   incrementViewsCount(tmp);
   lastTimeStamp = timeStamp;
-  
 
-  requestAnimationFrame(continuousGrowth);  //Loop
+  requestAnimationFrame(updateGame); //Loop
+
+  updateButton();
+  passiveGrowthDisplay.innerText = `VIEWS PER SECOND: ${counterGrowthRate}`;
 }
-requestAnimationFrame(continuousGrowth);  //Start the animation loop
 
+function updateButton(): void {
+  if (viewsCount < 10) {
+    upgrade1.disabled = true;
+  } else {
+    upgrade1.disabled = false;
+  }
+}
+
+// Variables
+let viewsCount: number = 0; //Global counter variable
+let counterGrowthRate: number = 0; //Default value should be 0
+let lastTimeStamp: number = 0; //For continuous growth
+
+//Adding counter
+const viewsDisplay = addText(`VIEWS: ${viewsCount.toFixed(2)}`); //Text display
+button.addEventListener("click", () => {
+  //Implementing clicking increment by 1
+  incrementViewsCount(1);
+});
+
+//Adding passive incrementing
+const passiveGrowthDisplay = addText(`VIEWS PER SECOND: ${counterGrowthRate}`); //auto clicking display
+
+requestAnimationFrame(updateGame); //Starts the loop to run every frame
+
+//Adding a buy option for auto clicking
+const upgrade1 = document.createElement("button");
+upgrade1.innerText = "FAN 10 VIEWS\n 0.1 VPS";
+app.append(upgrade1);
+
+upgrade1.addEventListener("click", () => {
+  incrementViewsCount(-10);
+  counterGrowthRate += 1;
+});
