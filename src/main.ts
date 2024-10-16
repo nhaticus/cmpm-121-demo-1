4b2dcbbeaf139133e06fb7cad718203ca0608f35
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-10-16 00:00:43 -0700

    Step 3 Complete + comments

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,35 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "The Influencer Program";
 document.title = gameName;
 
+//header
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 header.style.color = "#FACADE";
 app.append(header);
 
+//Adding button that will be doing the incrementing
 const button = document.createElement("button");
 button.innerText = "▶️";
 button.style.backgroundColor = "red";
 app.append(button);
+
+//Adding counter
+let viewCounter: number = 0;
+const viewDisplay = document.createElement("div");  //Text display
+app.append(viewDisplay);
+viewDisplay.innerHTML = "VIEWS: " + viewCounter;
+button.addEventListener('click', () => {  //increment counter and update text on click
+  viewCounter++;
+  update();
+})
+
+//Adding passive incrementing
+let passiveCounter: number = 1;
+setInterval(() => {
+  viewCounter += passiveCounter;
+  update();
+}, 1000);
+
+function update() :void{
+  viewDisplay.innerHTML = "VIEWS: " + viewCounter;
+}
