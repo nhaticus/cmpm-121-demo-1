f809f29bb0580adfcb40d55aa37cfe6970a9b285
Nhat Thai <nhatthai73@gmail.com>
2024-10-18 10:56:32 -0700

    Step 8 Complete + organized

--- src/main.ts
+++ src/main.ts
@@ -11,31 +11,45 @@ header.innerHTML = gameName;
 header.style.color = "white";
 app.append(header);
 
-//Adding button that will be doing the incrementing
-const button = document.createElement("button");
-button.innerText = "▶️";
-button.style.backgroundColor = "red";
-app.append(button);
-
-// Helper functions
-function incrementViewsCount(x: number): void {
-  viewsCount += x;
-  viewsDisplay.innerText = `\nVIEWS: ${viewsCount.toFixed(2)}\n`;
-  updateButtons(); // Check if upgrades is available when number grows
-}
-
-function incrementRates(x: number): void {
-  counterGrowthRate += x;
-  passiveGrowthDisplay.innerText = `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`;
-}
-
 function addText(text: string): HTMLDivElement {
   const textBox = document.createElement("div");
   textBox.innerText = text;
   app.append(textBox);
   return textBox;
 }
+
+//Adding button that will be doing the incrementing
+const button = document.createElement("button");
+button.innerText = "▶️";
+button.style.backgroundColor = "red";
+app.append(button);
+button.addEventListener("click", () => {
+  //Implementing clicking increment by 1
+  incrementViewsCount(1);
+});
+
+let viewsCount: number = 0; //Global counter variable
+//Adding display for views
+const viewsDisplay = addText(`\nVIEWS: ${viewsCount.toFixed(2)}\n`); //Text display
+
+function incrementViewsCount(x: number): void {
+  viewsCount += x;
+  viewsDisplay.innerText = `\nVIEWS: ${viewsCount.toFixed(2)}\n`;
+  updateButtons(); // Check if upgrades is available when number grows
+}
+
+let counterGrowthRate: number = 0; //Default value should be 0
+const passiveGrowthDisplay = addText(
+  `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`
+);
+
+function incrementRates(x: number): void {
+  counterGrowthRate += x;
+  passiveGrowthDisplay.innerText = `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`;
+}
+
 //Use for updating increments every frame also for anything that needs to be updated each frame
+let lastTimeStamp: number = 0;
 function updateGame(timeStamp: number): void {
   if (lastTimeStamp === 0) {
     lastTimeStamp = timeStamp;
@@ -50,23 +64,7 @@ function updateGame(timeStamp: number): void {
 
 requestAnimationFrame(updateGame); //Starts the loop to run every frame
 
-// Variables
-let viewsCount: number = 0; //Global counter variable
-let counterGrowthRate: number = 0; //Default value should be 0
-let lastTimeStamp: number = 0; //For continuous growth
-
-//Adding counter
-const viewsDisplay = addText(""); //Text display
-button.addEventListener("click", () => {
-  //Implementing clicking increment by 1
-  incrementViewsCount(1);
-});
-
-//Adding passive incrementing
-const passiveGrowthDisplay = addText(
-  `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`
-); //auto clicking display
-
+// Creating Upgrade interface with useful properties
 interface Upgrade {
   name: string;
   cost: number;
@@ -74,10 +72,11 @@ interface Upgrade {
   level: number;
 }
 
+// List of Upgrades
 const upgradeList: Upgrade[] = [
-  { name: "Fan(s)", cost: 10, rate: 0.1, level: 0 },
-  { name: "Collaboration(s)", cost: 100, rate: 2, level: 0 },
-  { name: "Bot(s)", cost: 1000, rate: 50, level: 0 },
+  { name: "Viewer(s)", cost: 10, rate: 0.1, level: 0 },
+  { name: "Fan(s)", cost: 100, rate: 2, level: 0 },
+  { name: "Collaboration(s)", cost: 1000, rate: 50, level: 0 },
 ];
 
 // Function to create buttons for each upgrade
@@ -102,7 +101,7 @@ function createUpgradeButtons() {
       upgradeContainer.appendChild(button);
 
       const rateDisplay = document.createElement("p");
-      rateDisplay.innerText = `Views/sec: ${upgrade.rate}`;
+      rateDisplay.innerText = `${upgrade.rate} Views/sec `;
       upgradeContainer.appendChild(rateDisplay);
 
       appDiv.appendChild(upgradeContainer);
