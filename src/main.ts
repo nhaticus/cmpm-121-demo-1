e8bed52bd284080a3fbe7efb05a18e460a2a9e7a
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-11-06 12:28:15 -0800

    D1.c cleanups

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,118 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "The Influencer Program";
-document.title = gameName;
+/* GLOBAL VARIABLES */
+let viewsCount: number;
+let counterGrowthRate: number;
+let viewsDisplay: HTMLDivElement;
+let passiveGrowthDisplay: HTMLDivElement;
+//Use for updating increments every frame also for anything that needs to be updated each frame
+let lastTimeStamp: number;
 
-//header
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-header.style.color = "white";
-app.append(header);
+interface Upgrade {
+  name: string;
+  cost: number;
+  rate: number;
+  level: number;
+  description: string;
+}
 
+interface GameConfig {
+  name: string;
+  styles: {
+    headerColor: string;
+    buttonColor: string;
+  };
+
+  initialState: {
+    views: number,
+    growthRate: number,
+  }
+
+  upgrades: Upgrade[]
+}
+
+const gameConfig: GameConfig = {
+  name: "The Influencer Program",
+  styles: {
+    headerColor: "white",
+    buttonColor: "red",
+  },
+
+  initialState: {
+    views: 0,
+    growthRate: 0,
+  },
+  upgrades: [
+    {
+      name: "Lurker(s)",
+      cost: 10,
+      rate: 0.1,
+      level: 0,
+      description: "WOW! an occasional viewer that watches once in a while",
+    },
+    {
+      name: "Subscriber(s)",
+      cost: 100,
+      rate: 2,
+      level: 0,
+      description:
+        "Someone finds you interesting and kept watching... that's rare",
+    },
+    {
+      name: "Collaboration(s)",
+      cost: 1000,
+      rate: 50,
+      level: 0,
+      description:
+        "A massive video collaboration with a famous STAR! look at those views!!! how did you land that",
+    },
+    {
+      name: "Superfan(s)",
+      cost: 10000,
+      rate: 1250,
+      level: 0,
+      description:
+        "Grow a cult of devoted followers that eat, sleep, watch you, and repeat",
+    },
+    {
+      name: "Bot(s)",
+      cost: 99999999.99,
+      rate: 9999999999,
+      level: 0,
+      description: "Really bro... BOTS??? you really fell off",
+    },
+  ],
+};
+
+function initGame(config: GameConfig) {
+  document.title = config.name;
+
+  /*  HEADER  */
+  const header = document.createElement("h1");
+  header.innerHTML = config.name;
+  header.style.color = config.styles.headerColor;
+  app.append(header);
+
+  /*  SETTING INITIAL VALUES */
+  lastTimeStamp = 0;
+
+  viewsCount = gameConfig.initialState.views;
+  counterGrowthRate = config.initialState.growthRate;
+
+  viewsDisplay = addText(`\nVIEWS: ${viewsCount.toFixed(2)}\n`);
+  passiveGrowthDisplay = addText(`VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`);
+
+  /*  CLICKER BUTTON FOR INCREMENTAL GAME */
+  createIncermentalButton("▶️", gameConfig);
+
+  /*  UPGRADE BUTTONS FOR THE GAME */
+  createUpgradeButtons(config)
+}
+
+initGame(gameConfig);
+
+// Creates a text using the paramater and return a text block element
 function addText(text: string): HTMLDivElement {
   const textBox = document.createElement("div");
   textBox.innerText = text;
@@ -19,18 +122,19 @@ function addText(text: string): HTMLDivElement {
 }
 
 //Adding button that will be doing the incrementing
-const button = document.createElement("button");
-button.innerText = "▶️";
-button.style.backgroundColor = "red";
-app.append(button);
-button.addEventListener("click", () => {
+function createIncermentalButton(text: string, config: GameConfig) {
+  const tmpButton = document.createElement("button");
+  tmpButton.innerText = text;
+  tmpButton.style.backgroundColor = config.styles.buttonColor;
+  app.append(tmpButton);
+
+  tmpButton.addEventListener("click", incrementalButtonHandler);
+}
+
+function incrementalButtonHandler() {
   //Implementing clicking increment by 1
   incrementViewsCount(1);
-});
-
-let viewsCount: number = 0; //Global counter variable
-//Adding display for views
-const viewsDisplay = addText(`\nVIEWS: ${viewsCount.toFixed(2)}\n`); //Text display
+}
 
 function incrementViewsCount(x: number): void {
   viewsCount += x;
@@ -38,18 +142,11 @@ function incrementViewsCount(x: number): void {
   updateButtons(); // Check if upgrades is available when number grows
 }
 
-let counterGrowthRate: number = 0; //Default value should be 0
-const passiveGrowthDisplay = addText(
-  `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`
-);
-
 function incrementRates(x: number): void {
   counterGrowthRate += x;
   passiveGrowthDisplay.innerText = `VIEWS/SEC: ${counterGrowthRate.toFixed(2)}\n\n`;
 }
 
-//Use for updating increments every frame also for anything that needs to be updated each frame
-let lastTimeStamp: number = 0;
 function updateGame(timeStamp: number): void {
   if (lastTimeStamp === 0) {
     lastTimeStamp = timeStamp;
@@ -64,63 +161,13 @@ function updateGame(timeStamp: number): void {
 
 requestAnimationFrame(updateGame); //Starts the loop to run every frame
 
-// Creating Upgrade interface with useful properties
-interface Upgrade {
-  name: string;
-  cost: number;
-  rate: number;
-  level: number;
-  description: string;
-}
-
-// List of Upgrades
-const upgradeList: Upgrade[] = [
-  {
-    name: "Lurker(s)",
-    cost: 10,
-    rate: 0.1,
-    level: 0,
-    description: "WOW! an occasional viewer that watches once in a while",
-  },
-  {
-    name: "Subscriber(s)",
-    cost: 100,
-    rate: 2,
-    level: 0,
-    description:
-      "Someone finds you interesting and kept watching... that's rare",
-  },
-  {
-    name: "Collaboration(s)",
-    cost: 1000,
-    rate: 50,
-    level: 0,
-    description:
-      "A massive video collaboration with a famous STAR! look at those views!!! how did you land that",
-  },
-  {
-    name: "Superfan(s)",
-    cost: 10000,
-    rate: 1250,
-    level: 0,
-    description:
-      "Grow a cult of devoted followers that eat, sleep, watch you, and repeat",
-  },
-  {
-    name: "Bot(s)",
-    cost: 99999999.99,
-    rate: 9999999999,
-    level: 0,
-    description: "Really bro... BOTS??? you really fell off",
-  },
-];
-
+/*  UPGRADE HANDLING FUNCTIONS  */
 // Function to create buttons for each upgrade
-function createUpgradeButtons() {
+function createUpgradeButtons(config: GameConfig) {
   const appDiv = document.getElementById("app");
 
   if (appDiv) {
-    upgradeList.forEach((upgrade, index) => {
+    config.upgrades.forEach((upgrade, index) => {
       // Create a container div for each upgrade
       const upgradeContainer = document.createElement("div");
 
@@ -156,7 +203,7 @@ function handleUpgrade(upgrade: Upgrade) {
 
 // Function to disable/enable buttons runs whenever views is incremented
 function updateButtons(): void {
-  upgradeList.forEach((upgrade, index) => {
+  gameConfig.upgrades.forEach((upgrade, index) => {
     const button = document.getElementById(
       `upgrade-${index}`
     ) as HTMLButtonElement;
@@ -166,6 +213,3 @@ function updateButtons(): void {
     }
   });
 }
-
-// Initialize buttons
-createUpgradeButtons();
